878b6fe95649c006c9127da3e724504849e21e71
Michael O'Loughlin <mpolough@cox.net>
2016-02-21 21:11:13 -0700

    Initial Version of Calculator.

---
+++ CSE360/src/cse360assign3/Calculator.java
@@ -0,0 +1,72 @@
+/**
+ * emulates a calculator
+ * 
+ * @author Michael O'Loughlin Pin#619
+ * @version Jan 26, 2015
+ * 
+ * 
+*/
+package cse360assign3;
+
+public class Calculator {
+	
+	/** creates a variable that keeps track of the total of all operations */
+	private int total;
+	
+	/** Constructor for every calculator object
+	 * 
+	 * 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** Gets the total of the calculator object
+	 * 
+	 * @return total 		the total for the calculator object
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** Adds the value to total
+	 * 
+	 * 
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** Subtracts the value from the total
+	 * 
+	 * 
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** multiplies the value to the total
+	 * 
+	 * 
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** divides the value from the total
+	 * 
+	 * 
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** returns the entire history of the calculator object
+	 * 
+	 * 
+	 */
+	public String getHistory () {
+		return "";
+	}
+
+}
---
+++ CSE360/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,59 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	@Test
+	public void CalculatorStub()
+	{
+		fail();
+		
+	}
+	
+	@Test
+	public void getTotalStub()
+	{
+		fail();
+		
+	}
+	
+	@Test
+	public void addStub()
+	{
+		fail();
+		
+	}
+	
+	@Test
+	public void subtractStub()
+	{
+		fail();
+		
+	}
+	
+	@Test
+	public void multiplyStub()
+	{
+		fail();
+		
+	}
+	
+	@Test
+	public void divideStub()
+	{
+		fail();
+		
+	}
+	
+	@Test
+	public void getHistoryStub()
+	{
+		Calculator dim = new Calculator();
+		assertNull(dim.getHistory());
+		
+	}
+
+}
